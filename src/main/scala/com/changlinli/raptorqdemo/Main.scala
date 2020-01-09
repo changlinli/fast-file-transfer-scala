b7ee71f15d3f38e806208169ce0075a56c70ccb3
Changlin Li <mail@changlinli.com>
2020-01-09 18:11:08 -0500

    Remove extraneous code

--- src/main/scala/com/changlinli/raptorqdemo/Main.scala
+++ src/main/scala/com/changlinli/raptorqdemo/Main.scala
@@ -10,18 +10,9 @@ import scala.collection.immutable.ArraySeq
 import scala.collection.immutable.ArraySeq.ofByte
 import scala.jdk.CollectionConverters._
 
-sealed trait DecodeEncodingPacketResult
-final case class FinishedDecoding(data: Array[Byte]) extends DecodeEncodingPacketResult
-final case class NotYetDone(newDecoderState: RaptorQDecoder) extends DecodeEncodingPacketResult
-
-final case class RaptorQDecoder(
-  fecParameters: FECParameters,
-  blocks: List[EncodingPacket]
-) {
-  def decodeCurrentElements(encodingPacket: EncodingPacket): DecodeEncodingPacketResult = {
-    ???
-  }
-}
+sealed trait BatchDecodeErrors
+final case class ProbabilisticDecodeFailure() extends BatchDecodeErrors
+final case class MalformedPackets() extends BatchDecodeErrors
 
 object ArraySeqUtils {
   def unsafeToByteArray(immutableByteArray: ArraySeq[Byte]): Array[Byte] = immutableByteArray match {
@@ -34,8 +25,8 @@ object ArraySeqUtils {
   }
 }
 
-object BulkRaptorQEncoder {
-  def splitUpDataAsSingleBlock(data: ArraySeq[Byte]): (FECParameters, LazyList[EncodingPacket]) = {
+object RaptorQEncoder {
+  def encodeAsSingleBlock(data: ArraySeq[Byte]): (FECParameters, LazyList[EncodingPacket]) = {
     val fecParameters = FECParameters.newParameters(data.length, 10, 1)
     val encoder = OpenRQ.newEncoder(
       ArraySeqUtils.unsafeToByteArray(data),
@@ -49,7 +40,7 @@ object BulkRaptorQEncoder {
     (fecParameters, packets)
   }
 
-  def splitUpData(data: ArraySeq[Byte], symbolSize: Int, numberOfSourceBlocks: Int): (FECParameters, LazyList[EncodingPacket]) = {
+  def encode(data: ArraySeq[Byte], symbolSize: Int, numberOfSourceBlocks: Int): (FECParameters, LazyList[EncodingPacket]) = {
     val fecParameters = FECParameters.newParameters(data.length, symbolSize, numberOfSourceBlocks)
     val topLevelEncoder = OpenRQ.newEncoder(
       ArraySeqUtils.unsafeToByteArray(data),
@@ -85,12 +76,12 @@ object BulkRaptorQEncoder {
   }
 }
 
-object BulkRaptorQDecoder {
-  def bulkDecode(allPackets: List[EncodingPacket], fecParameters: FECParameters): ArraySeq[Byte] = {
+object BatchRaptorQDecoder {
+  def batchDecode(allPackets: List[EncodingPacket], fecParameters: FECParameters): ArraySeq[Byte] = {
     val numberOfSourceBlocks = fecParameters.numberOfSourceBlocks
     val topLevelDecoder = OpenRQ.newDecoder(fecParameters, 5)
     val decoders = Range(0, numberOfSourceBlocks)
-      .map{
+      .map {
         sourceBlock =>
           val decoder = topLevelDecoder.sourceBlock(sourceBlock)
           sourceBlock -> decoder
@@ -105,24 +96,12 @@ object BulkRaptorQDecoder {
   }
 }
 
-final case class BulkRaptorQDecoder(
-  fecParameters: FECParameters,
-)
-
-final case class RaptorQEncoder() {
-  def generatePacket: (Array[Byte], RaptorQEncoder) = ???
-}
-
-object RaptorQDecoder {
-  def newDecoder(fecParameters: FECParameters): RaptorQDecoder = new RaptorQDecoder(fecParameters, List.empty)
-}
-
 object Main {
   def main(args: Array[String]): Unit = {
     val myBytes = ArraySeq.from(Range(1, 100).map(int => int.toByte))
-    val (fecParameters, encodedBytes) = BulkRaptorQEncoder.splitUpData(myBytes, 2, 5)
+    val (fecParameters, encodedBytes) = RaptorQEncoder.encode(myBytes, 2, 5)
     val loseALotOfEncodedBytes = encodedBytes.dropWhile(packet => packet.encodingSymbolID() % 2 == 0)
-    val result = BulkRaptorQDecoder.bulkDecode(loseALotOfEncodedBytes.take(1000).toList, fecParameters)
+    val result = BatchRaptorQDecoder.batchDecode(loseALotOfEncodedBytes.take(1000).toList, fecParameters)
     println(s"Hello world!: $result")
   }
 }
