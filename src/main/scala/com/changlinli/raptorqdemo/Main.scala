ee6d7a6d926c8844e7bf3df8a585dcbc9945cee6
Changlin Li <mail@changlinli.com>
2020-01-09 19:53:53 -0500

    Add UDP servers

--- src/main/scala/com/changlinli/raptorqdemo/Main.scala
+++ src/main/scala/com/changlinli/raptorqdemo/Main.scala
@@ -1,13 +1,21 @@
 package com.changlinli.raptorqdemo
 
+import java.net.InetSocketAddress
 import java.nio.ByteBuffer
+import java.util.concurrent.{ExecutorService, Executors, TimeUnit}
 
+import cats.implicits._
+import cats.effect.{Blocker, ExitCode, IO, IOApp, Resource}
+import fs2.Chunk
+import fs2.io.udp.{Packet, Socket, SocketGroup}
 import net.fec.openrq.{EncodingPacket, OpenRQ}
 import net.fec.openrq.parameters.{FECParameters, ParameterChecker}
 import net.fec.openrq.util.io.ByteBuffers
 
 import scala.collection.immutable.ArraySeq
 import scala.collection.immutable.ArraySeq.ofByte
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
 import scala.jdk.CollectionConverters._
 
 sealed trait BatchDecodeErrors
@@ -96,12 +104,97 @@ object BatchRaptorQDecoder {
   }
 }
 
-object Main {
-  def main(args: Array[String]): Unit = {
+object UdpProcessing {
+  def sendAsUdp(data: ArraySeq[Byte]): (FECParameters, LazyList[Packet]) = {
+    val (fecParameters, encodingPackets) = RaptorQEncoder.encodeAsSingleBlock(data)
+    val udpPackets = encodingPackets.map(
+      encodingPacket =>
+        Packet(new InetSocketAddress(8011), Chunk.bytes(encodingPacket.asArray()))
+    )
+    (fecParameters, udpPackets)
+  }
+
+  def fromUdpPacket(fecParameters: FECParameters, udpPacket: Packet): EncodingPacket = {
+    val decoder = OpenRQ.newDecoder(fecParameters, 5)
+    decoder
+      .parsePacket(udpPacket.bytes.toByteBuffer, true)
+      // FIXME
+      .value()
+  }
+
+  def fromUdp(fecParameters: FECParameters, udpPackets: LazyList[Packet]): LazyList[EncodingPacket] = {
+    val decoder = OpenRQ.newDecoder(fecParameters, 5)
+    udpPackets.map{
+      udpPacket =>
+        decoder
+          .parsePacket(udpPacket.bytes.toByteBuffer, true)
+          // FIXME
+          .value()
+    }
+  }
+}
+
+object UdpServer {
+
+  def listenToSocket(socket: Socket[IO]) = {
+    socket.reads()
+  }
+}
+
+object Main extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = {
     val myBytes = ArraySeq.from(Range(1, 100).map(int => int.toByte))
     val (fecParameters, encodedBytes) = RaptorQEncoder.encode(myBytes, 2, 5)
     val loseALotOfEncodedBytes = encodedBytes.dropWhile(packet => packet.encodingSymbolID() % 2 == 0)
     val result = BatchRaptorQDecoder.batchDecode(loseALotOfEncodedBytes.take(1000).toList, fecParameters)
     println(s"Hello world!: $result")
+
+    val blockerAndExecutorResource: Resource[IO, (Blocker, ExecutorService)] =
+      Resource.make{
+        IO{
+          val threadPool = Executors.newCachedThreadPool()
+          val blocker = Blocker.liftExecutionContext(ExecutionContext.fromExecutorService(threadPool))
+          (blocker, threadPool)
+        }
+      }{case (_, executorService) => IO(executorService.shutdown())}
+
+    val blockerResource: Resource[IO, Blocker] = blockerAndExecutorResource.map(_._1)
+
+    val (fecParameters1, udpPackets) = UdpProcessing.sendAsUdp(myBytes)
+
+    val action = if (args(1) == "r") {
+      blockerResource
+        .flatMap(blocker => SocketGroup[IO](blocker))
+        .flatMap(socketGroup => socketGroup.open(new InetSocketAddress(8011)))
+        .use{
+          socket =>
+            socket
+              .reads(Some(FiniteDuration(10, TimeUnit.SECONDS)))
+              .take(100)
+              .evalTap(packet => IO(println(s"Packet: $packet")))
+              .map(UdpProcessing.fromUdpPacket(fecParameters1, _))
+              .compile
+              .toList
+              .map(encodingPackets => BatchRaptorQDecoder.batchDecode(encodingPackets, fecParameters1))
+              .flatMap(bytes => IO(println(s"Our bytes were: $bytes")))
+        }
+    } else {
+      println(s"Our arg was ${args(1)}")
+      blockerResource
+        .flatMap(blocker => SocketGroup[IO](blocker))
+        .flatMap(socketGroup => socketGroup.open(new InetSocketAddress(8012)))
+        .use{
+          (socket: Socket[IO]) =>
+            udpPackets
+              .take(1000)
+              .toList
+              .traverse(udpPacket => socket.write(udpPacket, Some(FiniteDuration(1, TimeUnit.SECONDS))))
+        }
+    }
+    for {
+      _ <- action
+      _ <- IO(println("We're done with our action"))
+    } yield ExitCode.Success
   }
 }
